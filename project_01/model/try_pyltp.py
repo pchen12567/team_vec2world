0e2b6499530b4e04c98397133e164b8374d9ec49
pchen12567 <pchen12@stevens.edu>
2019-05-31 15:26:24 -0400

    update

--- project_01/model/try_pyltp.py
+++ project_01/model/try_pyltp.py
@@ -14,7 +14,6 @@ from pyltp import NamedEntityRecognizer
 from pyltp import Parser
 import jieba
 from pyltp import SementicRoleLabeller
-from collections import defaultdict
 
 # 初始化路径
 # ltp模型目录的路径
@@ -143,18 +142,18 @@ def extract_single_sentence(saying_list, sentence):
 
     if not match_words:
         print("----不包含'说'----")
-        return {}
+        return False
 
-    print("匹配到的说", match_words)
+    # print("匹配到的说", match_words)
 
     # print("词性标注情况")
     # print([(str(index) + words[index], pos) for index, pos in enumerate(postags)])
 
-    print("命名实体识别情况")
-    print([(str(index) + words[index], net) for index, net in enumerate(netags)])
+    # print("命名实体识别情况")
+    # print([(str(index) + words[index], net) for index, net in enumerate(netags)])
 
-    print("依存句法分析情况")
-    print([(str(index) + words[index], arc) for index, arc in enumerate(arcs)])
+    # print("依存句法分析情况")
+    # print([(str(index) + words[index], arc) for index, arc in enumerate(arcs)])
 
     names = get_names(words, netags)
     print('人名:', names)
@@ -166,40 +165,50 @@ def extract_single_sentence(saying_list, sentence):
 
 # 获取言论
 def get_opinions(words, match_words, names, arcs):
-    opinions = defaultdict(str)
+    opinions = []
 
-    for w in match_words:
-        saying_index = w[1]
+    for (name, position) in names:
+        start_index = position[0]
+        end_index = position[1]
 
-        head = arcs[saying_index][0]
-        relation = arcs[saying_index][1]
+        # if "：" in words:
+        #     print('condition one')
+        #     opinion_index = words.index('：')
+        #     opinion = ''.join(words[opinion_index + 1:])
+        #     opinions[name] = opinion
 
-        for (name, position) in names:
-            start = position[0]
-            end = position[1]
+        # 通过人名的 arc.relation = 'SBV' 找言论
+        if arcs[end_index][1] == 'SBV':
+            opinion_index = arcs[end_index][0]
 
-            if "：" in words:
-                print('condition one')
-                opinion_index = words.index('：')
-                opinion = ''.join(words[opinion_index + 1:])
-                opinions[name] = opinion
+            if arcs[opinion_index][1] == 'WP':
+                opinion_index += 1
 
-            elif head == start and relation == 'HED':
-                print('condition two')
-                opinion = ''.join(words[saying_index + 2:])
-                opinions[name] = opinion
+            opinion = ''.join(words[opinion_index:])
+            opinions.append((name, opinion))
 
-            elif arcs[head][1] == 'ATT' and relation == 'HED':
-                print('condition 3')
-                person_index = arcs[head][0]
-
-                if arcs[person_index][1] == 'WP':
-                    print('i am in')
-                    person_index += 1
-
-                if name.startswith(words[person_index]):
-                    opinion = ''.join(words[saying_index + 2:])
-                    opinions[name] = opinion
+        # for w in match_words:
+        #     saying_index = w[1]
+        #
+        #     head = arcs[saying_index][0]
+        #     relation = arcs[saying_index][1]
+        #
+        #     if head == start_index and relation == 'HED':
+        #         # print('condition two')
+        #         opinion = ''.join(words[saying_index + 2:])
+        #         opinions.append((name, opinion))
+        #
+        #     elif arcs[head][1] == 'ATT' and relation == 'HED':
+        #         # print('condition 3')
+        #         person_index = arcs[head][0]
+        #
+        #         if arcs[person_index][1] == 'WP':
+        #             print('i am in')
+        #             person_index += 1
+        #
+        #         if name.startswith(words[person_index]):
+        #             opinion = ''.join(words[saying_index + 2:])
+        #             opinions.append((name, opinion))
 
     return opinions
 
@@ -269,35 +278,36 @@ def main():
     #             print("人物：{}\n言论：{}".format(name, op))
     #     print('*' * 80)
 
-    # news = """在今天商务部举行的例行发布会上，有媒体表示：IMF近日发布的研究报告称，美国加征关税造成的成本几乎全部由美国企业承担了，但美国总统特朗普称，中国在为美国加征的关税买单。对此，新闻发言人高峰表示：美方的贸易霸凌主义最终损害的是美国自身，买单的是美国的消费者和企业。
-    #
-    # 　　高峰表示，中美贸易不平衡，主要是由于美国出口管制等非经济因素以及储蓄率低等原因造成的，加征关税根本解决不了贸易不平衡问题。
-    #
-    # 　　关于美国单方面加征关税的影响，高峰强调，美方的贸易霸凌做法最终损害的是美国自身，买单的是美国的消费者和企业，纽约联储经济学家最近的预测表明，美方加征关税措施，将使每个美国家庭每年平均损失831美元。
-    #
-    # 　　高峰表示，美国一些智库的研究也显示，如果美方的措施持续下去，会导致美国的GDP增速下滑、就业和投资减少、国内物价上升，美国商品在海外的竞争力下降，已经有越来越多的美国企业、消费者感受到加征关税的影响。
-    #
-    # 　　与此同时，高峰再次强调了中方关于中美经贸磋商的立场：中方绝不会接受任何有损国家主权和尊严的协议，在重大原则问题上，中方绝对不会让步，如果要达成协议，美方需要拿出诚意，妥善解决中方提出的核心关切，在平等相待、相互尊重的基础上继续磋商。
-    # """
+    news = """在今天商务部举行的例行发布会上，有媒体表示：IMF近日发布的研究报告称，美国加征关税造成的成本几乎全部由美国企业承担了，但美国总统特朗普称，中国在为美国加征的关税买单。对此，新闻发言人高峰表示：美方的贸易霸凌主义最终损害的是美国自身，买单的是美国的消费者和企业。
 
-    news = """美国贸易代表莱特希泽星期五表示，他已收到总统特朗普的指示，对其余尚未加征关税的所有中国商品加征关税，星期一将公布细节。此后特朗普发推特呼吁中国现在就采取行动(向美方让步)。关于中美下一步何时磋商，美财政部长姆努钦对媒体回应说：“目前还没有这方面的安排。”
+    　　高峰表示，中美贸易不平衡，主要是由于美国出口管制等非经济因素以及储蓄率低等原因造成的，加征关税根本解决不了贸易不平衡问题。
 
-    美国极限施压的大棒高高举起，中方的态度是：我们做好了应对各种情况的准备。“极限施压”与“应对各种情况”在博弈。
+    　　关于美国单方面加征关税的影响，高峰强调，美方的贸易霸凌做法最终损害的是美国自身，买单的是美国的消费者和企业，纽约联储经济学家最近的预测表明，美方加征关税措施，将使每个美国家庭每年平均损失831美元。
 
-    美方是挑起贸易战的一方，自恃实力强大，相信它的关税大棒足以压垮中国。中方的回应则是太极哲学的示范版，坚守原则，不主动打也不惧战，用更大的承受力瓦解对方的攻击力，增加其他情况出现的可能性。
+    　　高峰表示，美国一些智库的研究也显示，如果美方的措施持续下去，会导致美国的GDP增速下滑、就业和投资减少、国内物价上升，美国商品在海外的竞争力下降，已经有越来越多的美国企业、消费者感受到加征关税的影响。
 
-    美方攻势猛烈，但这种攻势在经济学上充满了非理性，对美国经济将导致常识性的自伤。美方显然寄希望于人类贸易史上从未有过的凶猛关税战可以一举摧毁中国的意志，以短时间内的损失换取中国接受不平等协议，铸就美国现政府的“辉煌大业”。美国的做法是赌博。美方不断发声，一个动作接一个动作，表现出的其实是他们急切希望这一切迅速生效的焦虑。
+    　　与此同时，高峰再次强调了中方关于中美经贸磋商的立场：中方绝不会接受任何有损国家主权和尊严的协议，在重大原则问题上，中方绝对不会让步，如果要达成协议，美方需要拿出诚意，妥善解决中方提出的核心关切，在平等相待、相互尊重的基础上继续磋商。
     """
+
+    # news = """美国贸易代表莱特希泽星期五表示，他已收到总统特朗普的指示，对其余尚未加征关税的所有中国商品加征关税，星期一将公布细节。此后特朗普发推特呼吁中国现在就采取行动(向美方让步)。关于中美下一步何时磋商，美财政部长姆努钦对媒体回应说：“目前还没有这方面的安排。”
+    #
+    # 美国极限施压的大棒高高举起，中方的态度是：我们做好了应对各种情况的准备。“极限施压”与“应对各种情况”在博弈。
+    #
+    # 美方是挑起贸易战的一方，自恃实力强大，相信它的关税大棒足以压垮中国。中方的回应则是太极哲学的示范版，坚守原则，不主动打也不惧战，用更大的承受力瓦解对方的攻击力，增加其他情况出现的可能性。
+    #
+    # 美方攻势猛烈，但这种攻势在经济学上充满了非理性，对美国经济将导致常识性的自伤。美方显然寄希望于人类贸易史上从未有过的凶猛关税战可以一举摧毁中国的意志，以短时间内的损失换取中国接受不平等协议，铸就美国现政府的“辉煌大业”。美国的做法是赌博。美方不断发声，一个动作接一个动作，表现出的其实是他们急切希望这一切迅速生效的焦虑。
+    # """
 
     sents_list = sentence_splitter(news)
     for sent in sents_list:
         print(sent)
         opinions = extract_single_sentence(saying_list, sent)
         if opinions:
-            for name, op in opinions.items():
+            print('提取成功')
+            for (name, op) in opinions:
                 print("人物：{}\n言论：{}".format(name, op))
         else:
-            print('无')
+            print('提取失败')
         print('*' * 80)
 
 
